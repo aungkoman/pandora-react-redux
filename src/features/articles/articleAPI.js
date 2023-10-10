4c9bcd17161be2e56963004e95bc59e0acfa2e8a
Aung Ko Man <aungkoman@gmail.com>
2023-10-10 14:36:45 +0630

    Update articleAPI.js

--- src/features/articles/articleAPI.js
+++ src/features/articles/articleAPI.js
@@ -44,3 +44,47 @@ export function articleSelectApi({ filter, page, accessToken }) {
     });
     */
 }
+
+
+// A mock function to mimic making an async request for data
+export function articleSelectApi({ filter, page, accessToken }) {
+
+    console.log("articleSelectApi");
+    console.log({ filter, page, accessToken });
+    
+
+    return new Promise((resolve, reject) => {
+        // TODO: filter need to be spread
+        let endpoint = ARTICLE_API_ENDPOINT + "?page=" + page;
+        const config = {
+            headers: {
+                'Authorization': `Bearer ${accessToken}`
+            }
+        };
+        axios.get(endpoint, config)
+            .then(response => {
+                setTimeout(() => resolve(response.data), 3000);
+                //resolve(response.data);
+            })
+            .catch(error => {
+                setTimeout(() => reject(error), 3000);
+                // reject(error);
+            });
+    });
+
+
+    // Send the POST request
+    /*
+    return axios.post(apiUrl, postData)
+    .then((response) => {
+      // Handle the response data here
+      console.log('Response:', response.data);
+      return response.data;
+    })
+    .catch((error) => {
+      // Handle any errors that occurred during the request
+      console.error('Error:', error);
+      return error;
+    });
+    */
+}
